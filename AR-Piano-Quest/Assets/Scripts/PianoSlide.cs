dbabeb2896c28a2fae313a242cfa06fd2ff08016
Jeremy Ting <ctin678@aucklanduni.ac.nz>
2024-09-03 14:36:58 +1200

    feat: completed grid

--- AR-Piano-Quest/Assets/Scripts/PianoSlide.cs
+++ AR-Piano-Quest/Assets/Scripts/PianoSlide.cs
@@ -9,7 +9,9 @@ public class PianoSlide : MonoBehaviour
 
     [SerializeField] Transform _background;
     [SerializeField] Transform _gridLineParent;
-    [SerializeField] GameObject _gridLineExample;
+    [SerializeField] GameObject _gridWhiteLineExample;
+    [SerializeField] GameObject _gridBlackLineExample;
+    [SerializeField] GameObject _gridBeatLineExample;
 
     [SerializeField] Color _whiteKeyColour = Color.white;
     [SerializeField] float _whiteKeyWidth = 0.022f;
@@ -22,23 +24,15 @@ public class PianoSlide : MonoBehaviour
     [SerializeField] float[] _blackToWhiteKeyOffsets = new float[7] { 0.013f, -1, 0.010f, 0.012f, -1, 0.009f, 0.011f };
     [SerializeField] float _blackKeyHover = 0.0002f;
 
-    KeyColumn[] _keyColumns;
-
     [SerializeField] float _unitLength = 0.1f;
-
-    bool _metronomeMode = false;
-    AudioSource _metronome;
-    float _last_tick_time;
     [SerializeField] int _metronomePerBeat = 1;
     [SerializeField] float _metronomeOffset = -0.2f;
 
     [SerializeField] BeatBar _beatBar;
-    [SerializeField] float _barLength = 0.001f;
     [SerializeField] float _barHover = 0.0003f;
 
-    bool _calibrationMode = false;
-    [SerializeField] Color _calibrationKeyColour = Color.blue;
-    [SerializeField] int[] _calibrationKeys = new int[] { 37, 40, 42, 45 };
+    int beatsPerBar = 4;
+    int barsPerSlide = 2;
 
     float width;
 
@@ -57,10 +51,11 @@ public class PianoSlide : MonoBehaviour
 
     void CreateGridLines()
     {
+        // White Lines
         for (int i = 0; i < _whiteKeyCount; i++)
         {
             // Instantiate a clone of the example visual
-            GameObject clone = Instantiate(_gridLineExample, _gridLineParent);
+            GameObject clone = Instantiate(_gridWhiteLineExample, _gridLineParent);
 
             // Enable the clone (in case the example is disabled)
             clone.SetActive(true);
@@ -73,9 +68,60 @@ public class PianoSlide : MonoBehaviour
 
             // Set the scale of the clone
             clone.transform.localScale = new Vector3(_whiteKeyWidth / 10, 1, _depth);
+        }
 
-            // Optionally, you can customize each clone (e.g., change color)
-            // clone.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, Color.black, (float)i / _whiteKeyCount);
+        // Black Lines
+        for (int i = 0; i < _whiteKeyCount; i++)
+        {
+            if (_blackToWhiteKeyOffsets[i % 7] < 0)
+                continue;
+
+            // Instantiate a clone of the example visual
+            GameObject clone = Instantiate(_gridBlackLineExample, _gridLineParent);
+
+            // Enable the clone (in case the example is disabled)
+            clone.SetActive(true);
+
+            // Calculate the x position based on the interval and index
+            float xPos = i * (_whiteKeyWidth + _whiteKeySpacing) - width / 2 + _whiteKeyWidth / 2 + _blackToWhiteKeyOffsets[i % 7];
+
+            // Set the position of the clone
+            clone.transform.localPosition = new Vector3(xPos, _barHover, _depth / 2);
+
+            // Set the scale of the clone
+            clone.transform.localScale = new Vector3(_blackKeyWidth / 10, 1, _depth);
+        }
+
+        // Beat Lines
+        for (int i = 0; i < beatsPerBar * barsPerSlide + 1; i++)
+        {
+            // Instantiate a clone of the example visual
+            GameObject clone = Instantiate(_gridBeatLineExample, _gridLineParent);
+
+            // Enable the clone (in case the example is disabled)
+            clone.SetActive(true);
+
+            // Set the position of the clone
+            clone.transform.localPosition = new Vector3(0, _barHover * 2, _depth * i / (beatsPerBar * barsPerSlide));
+
+            // Set the scale of the clone
+            clone.transform.localScale = new Vector3(width, 1, _whiteKeyWidth / 10);
+        }
+
+        // Bar Lines
+        for (int i = 0; i < barsPerSlide + 1; i++)
+        {
+            // Instantiate a clone of the example visual
+            GameObject clone = Instantiate(_gridBeatLineExample, _gridLineParent);
+
+            // Enable the clone (in case the example is disabled)
+            clone.SetActive(true);
+
+            // Set the position of the clone
+            clone.transform.localPosition = new Vector3(0, _barHover * 2, _depth * i / barsPerSlide);
+
+            // Set the scale of the clone
+            clone.transform.localScale = new Vector3(width, 1, _whiteKeyWidth / 5);
         }
     }
 
